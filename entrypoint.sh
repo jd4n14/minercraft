0dbee87dc14eedd999dbbb4f6ef36daddcca38f6
Diego <94983742+jd4n14@users.noreply.github.com>
2025-02-15 23:35:03 +0000

    add message

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 # Establece la variable PLUGINS con saltos de línea reales usando printf
+echo "starting server"
+
 export PLUGINS="$(printf '%s\n%s' \
   'https://download.geysermc.org/v2/projects/geyser/versions/latest/builds/latest/downloads/spigot' \
   'https://download.geysermc.org/v2/projects/floodgate/versions/latest/builds/latest/downloads/spigot')"
